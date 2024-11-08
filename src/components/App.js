7afd52942965f3a37d7e87ff95dc6917b93ca834
eunjin6-6 <dmswls616@gmail.com>
2024-11-08 15:09:52 +0900

    로그인까지 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { authService } from '../firebase';
 import AppRouter from './Router';
+import { authService } from '../firebase';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 console.log(authService);
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false); //로그인 확인 시작 여부
+
+  useEffect(()=>{
+  const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //유저 정보가 있다면-> Router에 전달해서 home
+        setIsLoggedIn(true);
+      } else {
+        //없다면 -> Router에 전달해서 login
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
+
   return (
-    <div>
-      <AppRouter isLoggedIn={isLoggedIn} />
-    </div>
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} />: "Initializing..." }
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -8,6 +8,7 @@ const Auth = ()=>{
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
 
   const auth = getAuth();
 
@@ -51,20 +52,37 @@ const Auth = ()=>{
       createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        // 계정생성(회원가입) 완료 후 할일
+        // 계정생성(회원가입) 완료 + 로그인도 완료(Storage에 로그인 정보 저장되어 있음)
         const user = userCredential.user; //생성된 계정의 유저정보 확인
         console.log(user);
       })
       .catch((error) => {
-        const errorCode = error.code;
+        //const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorCode, errorMessage)
+        // console.log(errorCode)
+        // console.log(errorMessage)
+        setError(errorMessage);
       });
     } else{
       //로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        //const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+      
     }
   }
 
+  const toggleAccount = ()=>{
+    setNewAccount(prev=>!prev);
+  }
 
   return(
     <div className="container">
@@ -78,8 +96,11 @@ const Auth = ()=>{
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" name="password" onChange={onChange} />
       </Form.Group>
-      <Button type="submit" variant="primary">Login</Button>
+      <Button type="submit" variant="primary">{newAccount ? '회원가입' : '로그인'}</Button>
+      <div className="mt-2">{error}</div>
     </Form>
+    <hr/>
+      <Button variant="dark" onClick={toggleAccount}>&#128123; {newAccount ? '로그인으로 전환' : '회원가입으로 전환'}</Button>
     </div>
   )
 }
