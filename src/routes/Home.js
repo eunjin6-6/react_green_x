2c6ceb5905c4c41b9dbe744ed5ca14bc265cf01c
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 09:56:01 +0900

    comment 입력폼

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { db } from '../firebase';
+import { collection, addDoc } from "firebase/firestore"; 
+
 
 const Home = ()=>{
+  const [comment, setComment] = useState('');
+  
+  const onChange = (e)=>{
+    // let value = e.target.value;
+    const {target:{value}} = e;
+    setComment(value);
+  }
+  const onSubmit = (e)=>{
+    e.preventDefault();
+  }
+
   return(
-    <div>Home</div>
+    <div className="container">
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">          
+          <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
+        </Form.Group>
+        <Button variant="primary">입력</Button>
+      </Form>
+    </div>
   )
 }
-
 export default Home;
