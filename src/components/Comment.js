36dcfcccaaec4e449097e7d68bb2ea0b67c7e39e
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 12:02:38 +0900

    글 작성자인지 확인 isOwener

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 
-const Comment = ({commentObj})=>{
+const Comment = ({commentObj, isOwener})=>{
   return(
     <ListGroup.Item>
       <div className="d-flex justify-content-between">
-        {commentObj.comment} 
-        <div className="d-flex gap-2">
-        <Button variant="secondary" size="sm">수정</Button>
-        <Button variant="danger" size="sm">삭제</Button>
-        </div>
+        {commentObj.comment}
+        {isOwener && 
+          <div className="d-flex gap-2">
+            <Button variant="secondary" size="sm">수정</Button>
+            <Button variant="danger" size="sm">삭제</Button>
+          </div>
+        } 
       </div>
       
     </ListGroup.Item>
