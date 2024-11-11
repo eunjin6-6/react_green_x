3753eb0048f969fd133a28e17765cbb713f02d21
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 11:02:17 +0900

    글 목록 조회, 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,25 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import { db } from '../firebase';
-import { collection, addDoc, serverTimestamp  } from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp, query, where, getDocs } from "firebase/firestore"; 
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const Home = ()=>{
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState(''); //입력하는 글 정보
+  const [comments, setComments] = useState([]); //조회된 글 정보들, 배열(리스트)
+
+  //글 리스트 조회
+  const getComments = async()=>{
+    const q = query(collection(db, "comments"));
+    const querySnapshot = await getDocs(q);
+    /*
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      const commentObj = {
+        ...doc.data(),
+        id:doc.id
+      }
+      //기존배열(prev)풀어헤치고, 넣어주기
+      setComments(prev=>[commentObj, ...prev]);
+    });
+    배열만 먼저 만들어주고, 한방에 넣는걸로 변경
+    */
+
+    //const commentArr = querySnapshot.docs.map(doc=>{return{...doc.data(), id:doc.id}})
+    const commentArr = querySnapshot.docs.map(doc=>({...doc.data(), id:doc.id}))
+    setComments(commentArr);
+  }
+  console.log(comments);
+
+  useEffect(()=>{
+    getComments();
+  },[]) //최초 렌더링 후 실행, 변동시 실행
   
   const onChange = (e)=>{
     // let value = e.target.value;
     const {target:{value}} = e;
     setComment(value);
   }
+
   const onSubmit = async (e)=>{
     e.preventDefault();
     console.log(comment, '실행');
     // Add a new document with a generated id.
     try{
       const docRef = await addDoc(collection(db, "comments"), {
-        conmment:comment,
+        comment:comment,
         date: serverTimestamp()
       });
       console.log("Document written with ID: ", docRef.id);
@@ -38,7 +69,13 @@ const Home = ()=>{
         </Form.Group>
         <Button type="submit" variant="primary">입력</Button>
       </Form>
+      <hr/>
+      <ListGroup>
+        {comments.map(item=>
+          <ListGroup.Item>{item.comment} </ListGroup.Item>
+        )}
+      </ListGroup>  
     </div>
-  )
+  );
 }
 export default Home;
