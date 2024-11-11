6ac48c9beaa9f22722b9e1a03454bd99165f961b
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 10:18:20 +0900

    addDoc로 문서 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import { db } from '../firebase';
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp  } from "firebase/firestore"; 
 
 
 const Home = ()=>{
@@ -13,17 +13,30 @@ const Home = ()=>{
     const {target:{value}} = e;
     setComment(value);
   }
-  const onSubmit = (e)=>{
+  const onSubmit = async (e)=>{
     e.preventDefault();
+    console.log(comment, '실행');
+    // Add a new document with a generated id.
+    try{
+      const docRef = await addDoc(collection(db, "comments"), {
+        conmment:comment,
+        date: serverTimestamp()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    }
+    catch(e){
+      console.log("Document written with ID: ", e);
+    }
+    
   }
 
   return(
     <div className="container">
       <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">          
-          <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
+        <Form.Group className="mb-3" controlId="comment">          
+          <Form.Control type="text" onChange={onChange} placeholder="글을 입력해주세요" />
         </Form.Group>
-        <Button variant="primary">입력</Button>
+        <Button type="submit" variant="primary">입력</Button>
       </Form>
     </div>
   )
