af30e6309427549899bdb917f2674030ffb8decd
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 09:56:25 +0900

    Firestore 초기화

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +19,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 //변수명 만들고, export 한꺼번에 쓰기
 //export const firebase = initializeApp(firebaseConfig);
 
 export const authService = getAuth(app);
+
+export const db = getFirestore(app);
