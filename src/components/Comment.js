2b09e53e183ad9e39c09d7113d874b13ee87d2fd
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 12:21:44 +0900

    삭제함수 로드

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import { doc, deleteDoc } from "firebase/firestore";
+
+const deleteComment = ()=>{
+  const deleteComfirm = window.confirm('정말 삭제할까요?')
+}
 
 const Comment = ({commentObj, isOwener})=>{
   return(
@@ -10,7 +15,7 @@ const Comment = ({commentObj, isOwener})=>{
         {isOwener && 
           <div className="d-flex gap-2">
             <Button variant="secondary" size="sm">수정</Button>
-            <Button variant="danger" size="sm">삭제</Button>
+            <Button variant="danger" onClick={deleteComment} size="sm">삭제</Button>
           </div>
         } 
       </div>
