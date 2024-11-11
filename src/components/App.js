3efa10618613c9eeec52cefaaf28f5fbc9ccb976
eunjin6-6 <dmswls616@gmail.com>
2024-11-11 12:02:16 +0900

    userObj 확인

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppRouter from './Router';
-import { authService } from '../firebase';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-console.log(authService);
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [init, setInit] = useState(false); //로그인 확인 시작 여부
+  const [userObj, setUserObj] = useState(null); //객체정보가 비어있는 초기값일때는 null
 
   useEffect(()=>{
   const auth = getAuth();
@@ -17,6 +16,8 @@ function App() {
       if (user) {
         //유저 정보가 있다면-> Router에 전달해서 home
         setIsLoggedIn(true);
+        //console.log(user);
+        setUserObj(user.uid);
       } else {
         //없다면 -> Router에 전달해서 login
         setIsLoggedIn(false);
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} />: "Initializing..." }
+      {init ? <AppRouter userObj={userObj} isLoggedIn={isLoggedIn} />: "Initializing..." }
     </>
   );
 }
