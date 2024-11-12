56c6deb6ea13c0028ea7e141efaf473d68cbe793
eunjin6-6 <dmswls616@gmail.com>
2024-11-12 10:09:05 +0900

    profile 이미지 변경

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import { getAuth, signOut } from "firebase/auth";
+import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const Profile = ()=>{
   const navigate = useNavigate();
-
-  const onLogoutClick = ()=>{
+  const onLogoutClick = () =>{
     const auth = getAuth();
+
     signOut(auth).then(() => {
       // Sign-out successful.
       alert('로그아웃 되었습니다.');
@@ -17,14 +18,17 @@ const Profile = ()=>{
       alert(error);
     });
   }
-
   return(
-    <div>
+    <div className='container'>
       <h1>Profile Page</h1>
-      <Button variant="danger" onClick={onLogoutClick}>로그아웃</Button>
+      <div className="profile">
+        <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt=""/>
+      </div>           
+        <input type="file" className="hidden" accept="image/*" name="profile" id="profile" />    
+        <label className="btn btn-primary" htmlFor="profile" >프로필 이미지 변경</label>      
+      <hr/>      
+      <Button variant="primary" onClick={onLogoutClick}>로그아웃</Button>
     </div>
-
   )
 }
-
 export default Profile;
