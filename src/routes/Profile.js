01f823d04e08900951591b2d0a49e7a7e907aeae
eunjin6-6 <dmswls616@gmail.com>
2024-11-12 13:16:16 +0900

    내 목록 글 수정 실시간반영

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { authService, db } from '../firebase';
-import { collection, query, orderBy, limit , getDocs, where } from "firebase/firestore"; 
+import { collection, query, orderBy, limit , where, onSnapshot } from "firebase/firestore"; 
 import ListGroup from 'react-bootstrap/ListGroup';
 import Comment from '../components/Comment';
 
@@ -17,6 +17,7 @@ const Profile = ()=>{
   const [comments, setComments] = useState([]); //조회된 글 배열
   const navigate = useNavigate();
 
+  /*
   const getComments = async ()=>{
     const q = query(collection(db, "comments"), where("uid", "==", user.uid), orderBy("date", "desc"), limit(5));
     const querySnapshot = await getDocs(q);
@@ -27,8 +28,23 @@ const Profile = ()=>{
   useEffect(()=>{
     getComments();
   },[]) //최소 렌더링후 실행, 변동시 실행
+*/
 
+  //글 리스트 조회
+  const getComments = async()=>{
 
+   //작성 시 실시간 반영
+   const q = query(collection(db, "comments"), where("uid", "==", user.uid), orderBy("date", "desc"), limit(5));
+   onSnapshot(q, (querySnapshot) => {
+     const commentArr = querySnapshot.docs.map(doc=>({...doc.data(), id:doc.id}))
+     setComments(commentArr);
+});
+ }
+ //console.log(comments);
+
+ useEffect(()=>{
+   getComments();
+ },[]) //최초 렌더링 후 실행, 변동시 실행
 
 
 
