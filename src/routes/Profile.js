4bb186fda5d1d25590c339354f63952f1faab480
eunjin6-6 <dmswls616@gmail.com>
2024-11-12 11:20:52 +0900

    로그아웃 버튼 위치 변경, commentList 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -61,9 +61,16 @@ const Profile = ()=>{
         <img src={profile} alt=""/>
       </div>           
         <input type="file" className="hidden" accept="image/*" name="profile" id="profile" onChange={updateLogo} />    
-        <label className="btn btn-primary" htmlFor="profile">프로필 이미지 변경</label>      
+        <div className="d-flex gap-1 mt-3">
+          <label className="btn btn-primary" htmlFor="profile">프로필 이미지 변경</label>
+          <Button variant="secondary" onClick={onLogoutClick}>로그아웃</Button>       
+        </div>
       <hr/>      
-      <Button variant="primary" onClick={onLogoutClick}>로그아웃</Button>
+      <h3>My Comment List</h3>
+      <ul className="commentList">
+        
+      </ul>
+     
     </div>
   )
 }
